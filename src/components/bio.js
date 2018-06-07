ee9152f2de355bb52967fda2d4440562c54b0a9d
Shari Hobbs <sharirhobbs@gmail.com>
2018-06-06 22:37:28 -0700

    redesign

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import Links from './links';
 import './bio.css';
 import '../grid.css';
 
 export default function Bio() {
   return (
-    <div className="row">
+    <div className="row bio-width">
       <div className="bio">
-        <h2>About me...</h2>
-        <p>After a career in customer service and operations management, I wanted to try something new. Web development is that thing. As soon as I finished my first project, I knew that Front End Development and designing delightful User Experiences is where I would land when I was finished learning. Of course, I soon found out that the learning, quite literally, never ends and that's been awesome. As it turns out, I love picking up new technologies and finding clever ways to work with the tools at hand. Any questions? Please contact me! Happy to talk about anything and everything...
+        <img className="image" src="https://user-images.githubusercontent.com/29738609/41066601-37ab420e-6997-11e8-9f39-c59e2a789d57.JPG"></img>
+        <h2 className="about">About me</h2>
+        <p className="bio-p">After a career in customer service and operations management, I wanted to try something new. Web development is that thing. As soon as I finished my first project, I knew that Front End Development and designing delightful user experiences is where I would land when I was finished learning. Of course, I soon found out that the learning, quite literally, never ends and that's been awesome. As it turns out, I love picking up new technologies and finding clever ways to work with the tools at hand.
         </p>
-       </div>
-       <div className="social-media">
-          <Links />
-        </div>
+      </div>
     </div>
   );
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,25 +5,37 @@ import '../grid.css';
 export default function Projects() {
   return (
     <div className="row">
+      <h2 className="projects-title">Recent Projects</h2>
       <div className="projects">
-        <h2>Recent Projects...</h2>
-        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/37367091-b1afc4aa-26bf-11e8-95fc-7a9d1e7f9a96.png" alt="OnHand"></img>
-        <p className="info">OnHand is a recipe search tool for the cook looking to make something new with what they already have on hand. A convenient, in-app shopping list keeps track of any additional ingredients needed for the user's next culinary adventure.
-        </p>
-        <a className="demo" href="https://sharihobbs.github.io/on-hand/" target="_blank">Live Demo Site</a>
-       </div>
-        <div className="projects">
-        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/40741813-5143605c-6401-11e8-8e8d-61e63daa178e.png" alt="Capture"></img>
-        <p className="info">Capture was inspired by the mindfulness practice of recognizing moments of gratitude throughout one's day. Keeping a log of what stirs joy or contentment is the best way to stay in a state of gratitude in even the most mundane moments.
-        </p>
-        <a className="demo" href="https://cryptic-plateau-80384.herokuapp.com/" target="_blank">Live Demo Site</a>
-       </div>
-        <div className="projects">
-        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/40741972-d10ec330-6401-11e8-9a53-7ae3e3558a18.png" alt="Tome"></img>
-        <p className="info">Tome was built because I found myself often forgetting the titles of books that were recommended in a magazine or by my son's teacher. With a handy note feature, you can keep track of where you first heard of a particular title, who might want to read it next, or when it needs to be in the mail to your Aunt Beth. Tome is simple, no-fuss, and utilizes the Google Books API Family to provide reliable, up to date information.
-        </p>
-        <a className="demo" href="https://sharp-swirles-724079.netlify.com/" target="_blank">Live Demo Site</a>
-       </div>
+        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/41078269-9ce70e80-69d0-11e8-932a-946b10b2392c.png" alt="OnHand"></img>
+        <div className="info">
+          <h3>OnHand</h3>
+          <p>A recipe search tool for the cook looking to make something new with what they already have on hand. A convenient, in-app shopping list keeps track of any additional ingredients needed for the user's next culinary adventure.
+          </p>
+          <a className="demo-code" href="https://sharihobbs.github.io/on-hand/" target="_blank">Live Demo Site{'  '}|{'  '}</a>
+          <a className="demo-code" href="https://github.com/sharihobbs/on-hand" target="_blank">See it on Github</a>
+        </div>
+      </div>
+      <div className="projects">
+        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/41078306-d2398bda-69d0-11e8-83b9-300c29d077a1.png" alt="Capture"></img>
+        <div className="info">
+          <h3>Capture</h3>
+          <p>An app inspired by the mindfulness practice of recognizing moments of gratitude throughout one's day. Keeping a log of what stirs joy or contentment is the best way to stay in a state of gratitude in even the most mundane moments.
+          </p>
+          <a className="demo-code" href="https://cryptic-plateau-80384.herokuapp.com/" target="_blank">Live Demo Site{'  '}|{'  '}</a>
+          <a className="demo-code" href="https://github.com/sharihobbs/capture" target="_blank">See it on Github</a>
+        </div>
+      </div>
+      <div className="projects">
+        <img className="screenshot" src="https://user-images.githubusercontent.com/29738609/41078324-e8f6dd78-69d0-11e8-9820-d6d319115c68.png" alt="Tome"></img>
+        <div className="info">
+        <h3>Tome</h3>
+          <p>Built to keep track of books that were recommended by friends, family, or in a magazine, online, etc. With the handy note feature, you can keep track of where you first heard of a particular title or who might want to read it next. Tome is simple, easy to reference, and utilizes the Google Books API Family to provide reliable, up to date information.
+          </p>
+          <a className="demo-code" href="https://sharp-swirles-724079.netlify.com/" target="_blank">Live Demo Site{'  '}|{'  '}</a>
+          <a className="demo-code" href="https://github.com/sharihobbs/tome-client" target="_blank">See it on Github</a>
+        </div>
+      </div>
     </div>
   );
 }
